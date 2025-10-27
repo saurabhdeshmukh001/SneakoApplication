d98500287ccc479807f33054810eb5f6817e58bb
Deshmukh <2440671@cognizant.com>
2025-10-27 12:09:53 +0530

    first commit

--- src/main/java/org/genc/app/SneakoAplication/service/api/AuthService.java
+++
@@ -1,9 +0,0 @@
-package org.genc.app.SneakoAplication.service.api;
-
-import org.genc.app.SneakoAplication.dto.AuthResponse;
-import org.genc.app.SneakoAplication.dto.RegisterRequest;
-
-public interface AuthService {
-    AuthResponse authenticate(String username, String password);
-    AuthResponse register(RegisterRequest request);
-}
